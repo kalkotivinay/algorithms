837192d7375c465bb9a815ecc9de05d1bc02e661
kalkotivinay <kalkoti.vinay@gmail.com>
2021-07-11 14:40:24 +0000

    Updated double_linked_list.c

--- double_linked_list.c
+++ double_linked_list.c
@@ -11,7 +11,7 @@ NODE *head = NULL;
 
 
 
-void add(NODE **head, int data)
+void add_first(NODE **head, int data)
 {
     NODE *new_node = (NODE *)malloc(sizeof(NODE));
     if (new_node == NULL)
@@ -23,23 +23,55 @@ void add(NODE **head, int data)
     return;
 }
 
+void add_last(NODE **head, int data)
+{
+    NODE **tracer = head;
+    printf("head %p, tracer %p, *tracer %p\n", head, tracer, *tracer);
+    NODE *new_node = (NODE *)malloc(sizeof(NODE));
+    if (new_node == NULL)
+        return;
+    
+    while (*tracer) {
+        tracer = &(*tracer)->next;
+    }
+    printf("head %p, tracer %p, *tracer %p\n", head, tracer, *tracer);
+    new_node->next = *tracer;
+    printf("new_node ptr %p\n", new_node);
+    printf("head %p, tracer %p, *tracer %p\n", head, tracer, *tracer);
+    new_node->data = data;
+    *tracer = new_node;
+    printf("head %p, tracer %p, *tracer %p\n", head, tracer, *tracer);
+    printf("============================\n");
+    return;
+}
+
 void dump_list(NODE **head)
 {
     NODE *temp = *head;
     while (temp != NULL) {
-        printf("%d\n", temp->data);
+        printf("data = %d, ptr = %p\n", temp->data, temp);
         temp = temp->next;
     }
 }
 
 int main()
 {
-    add(&head, 10);
-    add(&head, 20);
-    add(&head, 30);
-    add(&head, 32);
-    add(&head, 34);
-    add(&head, 21);
+/*
+    add_first(&head, 10);
+    add_first(&head, 20);
+    add_first(&head, 30);
+    add_first(&head, 32);
+    add_first(&head, 34);
+    add_first(&head, 21);
+*/
+
+    add_last(&head, 10);
+    add_last(&head, 20);
+    add_last(&head, 30);
+    //add_last(&head, 32);
+    //add_last(&head, 34);
+    //add_last(&head, 21);
+    printf("head %p, &head %p\n", head, &head);
     dump_list(&head);
     return 0;
 
