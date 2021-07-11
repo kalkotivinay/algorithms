306dbb42d9ad041e0287b21f89ad58af428e1ab6
kalkotivinay <kalkoti.vinay@gmail.com>
2021-07-11 09:12:24 +0000

    Updated double_linked_list.c

--- double_linked_list.c
+++ double_linked_list.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node{
+    struct node * next;
+    int data;
+}NODE;
+
+NODE *head = NULL;
+
+
+
+
+void add(NODE **head, int data)
+{
+    NODE *new_node = (NODE *)malloc(sizeof(NODE));
+    if (new_node == NULL)
+        return;
+
+    new_node->next = *head;
+    new_node->data = data;
+    *head = new_node;
+    return;
+}
+
+void dump_list(NODE **head)
+{
+    NODE *temp = *head;
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    add(&head, 10);
+    add(&head, 20);
+    add(&head, 30);
+    add(&head, 32);
+    add(&head, 34);
+    add(&head, 21);
+    dump_list(&head);
+    return 0;
+
+}
